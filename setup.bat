66e08fa1d0985cd95ab049c177dbfb6c368a906b
vidurbh <vidurbh@gmail.com>
2024-08-03 15:02:29 +0530

    virtual environment

---
+++ setup.bat
@@ -0,0 +1,12 @@
+:: Create and activate virtual environment
+python -m venv venv
+call venv\Scripts\activate
+
+:: Install dependencies
+pip install -r requirements.txt
+
+:: Run tests
+pytest -v -s -m "sanity" --browser Chrome --html=Reports/report.html 
+pytest -v -s .\TestCases\test_selectProduct.py --browser Chrome --html=Reports/report.html
+pytest -v -s .\TestCases\test_navigate.py --browser Chrome --html=Reports/report.html 
+pytest -v -s .\TestCases\test_productsearch.py --browser Chrome --html=Reports/report.html
